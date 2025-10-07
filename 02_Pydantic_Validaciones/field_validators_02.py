487ee99e3aaa91a319385ca890308cbb81ef2e7c
t0uxe <t0uxe@tuta.io>
2025-10-07 23:16:13 +0200

    Validación en clases, modificación del input después de haber sido validado, comprobación de elemento a elemento en una lista

--- 02_Pydantic_Validaciones/field_validators_02.py
+++ 02_Pydantic_Validaciones/field_validators_02.py
@@ -8,6 +8,7 @@ from pydantic import AfterValidator, BaseModel
 from typing import Annotated
 
 
+# =================== ANNOTATED ===================
 def es_par(value: int) -> int:
     if value % 2 == 1:
         raise ValueError(f"{value} no es número par.")
@@ -21,7 +22,17 @@ class Model1(BaseModel):
     my_number: NumeroPar
 
 
-ejemplo: Model1 = Model1(my_number=3)
+class Model2(BaseModel):
+    other_number: Annotated[NumeroPar, AfterValidator(lambda v: v + 2)]
 
-# class Model2(BaseModel):
-# class Model3(BaseModel):
+
+class Model3(BaseModel):
+    lista_pares: list[NumeroPar]
+
+
+# ejemplo: Model1 = Model1(my_number=3)
+# ejemplo2: Model2 = Model2(other_number=4)
+# print(ejemplo2)
+
+ejemplo3: Model3 = Model3(lista_pares=[2, 5, 10])
+print(ejemplo3)
