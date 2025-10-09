9f2766d2b2a4cccfef958b8fc7e15ac4f2199d73
t0uxe <t0uxe@tuta.io>
2025-10-09 12:00:02 +0200

    Validación comprobando restricciones de las cadenas, de máximo, mínima longitud, o de un patrón de regex usando regex

--- 03_Fastapi_Validaciones/query_validations_01.py
+++ 03_Fastapi_Validaciones/query_validations_01.py
@@ -5,8 +5,16 @@ from typing import Annotated
 app = FastAPI()
 
 
+# VALIDACIONES PARA STRINGS
+# max_length
+# min_length
+# pattern
+
+
 @app.get("/items/")
-async def read_items(q: Annotated[list[str] | None, Query()] = None):
+async def read_items(
+    q: Annotated[list[str] | None, Query(max_length=3)] = None,
+):
     results: dict = {"mensaje": "Acceso a get(read_items)"}
     if q:
         results.update({"q": q})
