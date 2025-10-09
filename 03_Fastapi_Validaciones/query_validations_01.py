80d08f418fd2cd7cf349034a3aba5f1fe27ac4b9
t0uxe <t0uxe@tuta.io>
2025-10-09 15:40:27 +0200

    Validacion gt lt le ge, y validacion personalizadas mediante el uso de una funcion externa para comprobar lo que necesitemos.

--- 03_Fastapi_Validaciones/query_validations_01.py
+++ 03_Fastapi_Validaciones/query_validations_01.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Query
 from typing import Annotated
+from pydantic import AfterValidator
 
 
 app = FastAPI()
@@ -11,9 +12,37 @@ app = FastAPI()
 # pattern
 
 
+def check_valid_id(id: str):
+    if id % 2 != 0:
+        raise ValueError("Necesita ser par")
+    return id
+
+
+# Mira que el valor sea mayor que 3
+# @app.get("/items/")
+# async def read_items(
+#     q: Annotated[int | None, Query(gt=3)] = None,
+# ):
+#     results: dict = {"mensaje": "Acceso a get(read_items)"}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+# Validacion para un valor mayor que X
+# @app.get("/items/")
+# async def read_items(
+#     q: Annotated[int | None, Query(gt=3)] = None,
+# ):
+#     results: dict = {"mensaje": "Acceso a get(read_items)"}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+
+# Validacion personalizada
 @app.get("/items/")
 async def read_items(
-    q: Annotated[list[str] | None, Query(max_length=3)] = None,
+    q: Annotated[int | None, AfterValidator(check_valid_id)] = None,
 ):
     results: dict = {"mensaje": "Acceso a get(read_items)"}
     if q:
